4882b1c8a3a6fca9ca4c61fbc7371c1d3375e12c
raysinensis <raysinensis@gmail.com>
2017-05-25 18:17:15 -0400

    email setup

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 # mail server settings
-MAIL_SERVER = 'localhost'
-MAIL_PORT = 25
-MAIL_USERNAME = None
-MAIL_PASSWORD = None
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'bot4rf'
+MAIL_PASSWORD = 'botbotbot'
 
 # administrator list
 ADMINS = ['rf@raysinensis.com']
